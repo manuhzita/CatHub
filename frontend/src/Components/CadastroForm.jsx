bbf41854bbe7de88aca7150497c8c78136a65175
Manoela Cruz <mitskiws@gmail.com>
2024-06-04 16:45:12 -0300

    arquivos alterados

--- frontend/src/Components/CadastroForm.jsx
+++ frontend/src/Components/CadastroForm.jsx
@@ -1,58 +1,106 @@
 // CadastroForm.jsx
-import React, { useState } from 'react';
-import axios from 'axios';
+import React from 'react'
+import 'rsuite/dist/rsuite.min.css';
+import { Form, ButtonToolbar, Button, Input } from 'rsuite';
+import { Modal } from 'rsuite';
+import { Tabs } from 'rsuite';
+import '../css/modalogin.css'
+import { Link } from 'react-router-dom';
+import CatLogo from '../img/cathub-removebg-preview.png'
 
-const CadastroForm = () => {
-  const [formData, setFormData] = useState({
-    nome: '',
-    email: '',
-    cpf: '',
-    endereco: '',
-    telefone: '',
-    senha: ''
-  });
+const ModalLogin = () => {
+  const [open, setOpen] = React.useState(false);
+  const [rows, setRows] = React.useState(0);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value
-    });
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      await axios.post('http://localhost:3001/cadastros', formData);
-      alert('Cadastro criado com sucesso!');
-      // Limpar o formulário após o envio bem-sucedido
-      setFormData({
-        nome: '',
-        email: '',
-        cpf: '',
-        endereco: '',
-        telefone: '',
-        senha: ''
-      });
-    } catch (error) {
-      console.error('Erro ao criar cadastro:', error);
-      alert('Erro ao criar cadastro. Verifique o console para mais detalhes.');
-    }
+  const handleEntered = () => {
+    setTimeout(() => setRows(80), 2000);
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input type="text" name="nome" placeholder="Nome" value={formData.nome} onChange={handleChange} />
-      <input type="email" name="email" placeholder="Email" value={formData.email} onChange={handleChange} />
-      <input type="text" name="cpf" placeholder="CPF" value={formData.cpf} onChange={handleChange} />
-      <input type="text" name="endereco" placeholder="Endereço" value={formData.endereco} onChange={handleChange} />
-      <input type="number" name="telefone" placeholder="Telefone" value={formData.telefone} onChange={handleChange} />
-      <input type="password" name="senha" placeholder="Senha" value={formData.senha} onChange={handleChange} />
-      <button type="submit">Salvar</button>
-    </form>
+    <>
+      <ButtonToolbar>
+        <Button onClick={handleOpen}>Open</Button>
+      </ButtonToolbar>
+
+      <Modal
+        open={open}
+        onClose={handleClose}
+        onEntered={handleEntered}
+        onExited={() => {
+          setRows(0);
+        }}
+      >
+        <Modal.Header >
+
+        </Modal.Header>
+        <Modal.Body>
+          <Tabs defaultActiveKey="1" appearance="subtle">
+            <Tabs.Tab eventKey="1" title="Login">
+              <br />
+              <div className='d-flex login'>
+                <h3 className='titulomodallogin'>Login</h3>
+                <img className='logotitulologin' src={CatLogo} />
+
+              </div>
+              <br />
+              <br />
+              <Form >
+                <Form.Group controlId="name">
+                  <Form.ControlLabel>Username</Form.ControlLabel>
+                  <Form.Control name="name" />
+                </Form.Group>
+
+                <Form.Group controlId="password">
+                  <Form.ControlLabel>Password</Form.ControlLabel>
+                  <Form.Control name="password" type="password" autoComplete="off" />
+                </Form.Group>
+                <Form.Group>
+                  <ButtonToolbar>
+                    <br />
+                    <Link appearance="default">Esqueceu a senha?</Link>
+                    <br />
+                    <Button className='modalloginbutton' appearance="primary">Entrar</Button>
+
+                  </ButtonToolbar>
+                </Form.Group>
+              </Form>
+            </Tabs.Tab>
+            <Tabs.Tab eventKey="2" title="Cadastro">
+              <Form>
+                <div className='d-flex cadastro'>
+                  <h3 className='titulomodalcadastro'>Cadastro</h3>
+                  <img className='logotitulocadastro' src={CatLogo} />
+
+                </div>
+                <Form.Group controlId="name">
+                  <Form.ControlLabel>Username</Form.ControlLabel>
+                  <Form.Control name="name" />
+
+                </Form.Group>
+                <Form.Group controlId="email">
+                  <Form.ControlLabel>Email</Form.ControlLabel>
+                  <Form.Control name="email" type="email" />
+
+                </Form.Group>
+                <Form.Group controlId="password">
+                  <Form.ControlLabel>Password</Form.ControlLabel>
+                  <Form.Control name="password" type="password" autoComplete="off" />
+                </Form.Group>
+                <Form.Group>
+                  <ButtonToolbar>
+                    <Button className='modalloginbutton'> Criar conta</Button>
+                  </ButtonToolbar>
+                </Form.Group>
+              </Form>
+            </Tabs.Tab>
+
+          </Tabs>
+        </Modal.Body>
+      </Modal>
+    </>
   );
 };
 
-export default CadastroForm;
-
-//meu penis está ereto
+export default ModalLogin;
